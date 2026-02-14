cb2edb677722fcd42695f82c9d468a4df1ae53a9
Lukas Müller <lukas.mueller@dozent.ipso.ch>
2026-02-14 20:37:59 +0100

    refactor basic topic

--- BD1_Basics/analyze.ps1
+++
@@ -1,9 +0,0 @@
-
-# Analyze sensor big data
-$data = Import-Csv sensordaten.csv
-
-$errors = $data | Where-Object { $_.Status -eq 'ERROR' }
-
-$stats = $errors | Group-Object Machine | Select-Object Name, Count
-
-$stats | Export-Csv error_stats.csv -NoTypeInformation
